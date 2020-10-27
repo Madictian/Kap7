6fdb7efdbc4c24942510f0566d754015c4dc4332
Madictian <fredrikbille@hotmail.com>
2020-10-27 08:45:42 +0100

    farver

--- src/Kap7Grades.java
+++ src/Kap7Grades.java
@@ -6,8 +6,8 @@ public class Kap7Grades {
         System.out.println("how many students: ");
         String[] classes = new String[scanner.nextInt()];
         for(int i = 0; i < classes.length;i++){
-            classes[i] = scanner.nextLine();
             System.out.println("Student: " + i);
+            classes[i] = scanner.next();
         }
         for(int n = 0; n < classes.length; ++n){
             System.out.println(classes[n]);
